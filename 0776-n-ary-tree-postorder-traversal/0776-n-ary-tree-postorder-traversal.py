f059e0e7f62a57ab70ae02c6e1804904ff4337f0
kalebted <115733387+kalebted@users.noreply.github.com>
2024-08-26 21:53:02 +0300

    Time: 44 ms (66.5%), Space: 18.1 MB (95.62%) - LeetHub

---
+++ 0776-n-ary-tree-postorder-traversal/0776-n-ary-tree-postorder-traversal.py
@@ -0,0 +1,20 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        output = []
+        while stack:
+            temp = stack.pop()
+            output.append(temp.val)
+            for child in temp.children:
+                stack.append(child)
+        return output[::-1]
