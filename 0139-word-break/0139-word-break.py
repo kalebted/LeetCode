2a584c6165b28590487d3ae593ff0c700ea0e96d
Kaleb Tedla <115733387+kalebted@users.noreply.github.com>
2024-09-05 10:56:28 +0300

    Time: 51 ms (8.05%), Space: 16.6 MB (51.65%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -3,11 +3,9 @@ class Solution:
         n = len(s) + 1
         dp = [False] * n
         dp[0] = True
-        trues = [0]
         for i in range(n):
-            for j in trues:
-                if s[j:i] in wordDict:
+            for j in range(i):
+                if dp[j] and s[j:i] in wordDict:
                     dp[i] = True
-                    trues.append(i)
                     break
         return dp[-1]
