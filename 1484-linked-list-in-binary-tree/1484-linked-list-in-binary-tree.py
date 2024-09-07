b90d8d5d3644f7d281b4bbdb9c907c137b90e170
Kaleb Tedla <115733387+kalebted@users.noreply.github.com>
2024-09-07 09:28:19 +0300

    Time: 73 ms (39.19%), Space: 17.2 MB (93.15%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.py
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.py
@@ -11,17 +11,7 @@
 #         self.right = right
 class Solution:
     def isSubPath(self, head: Optional[ListNode], root: Optional[TreeNode]) -> bool:
-        def helper(list_node, tree_node):
-            if not list_node:
-                return True
-            if not tree_node or list_node.val != tree_node.val:
-                return False
-            return(
-                helper(list_node.next, tree_node.left) or
-                helper(list_node.next, tree_node.right)
-            )
-        
-        if helper(head, root):
+        if self.helper(head, root):
             return True
         if not root:
             return False
@@ -29,4 +19,14 @@ class Solution:
         return (
             self.isSubPath(head, root.left) or
             self.isSubPath(head, root.right)
+        )
+
+    def helper(self, list_node, tree_node):
+        if not list_node:
+            return True
+        if not tree_node or list_node.val != tree_node.val:
+            return False
+        return(
+            self.helper(list_node.next, tree_node.left) or
+            self.helper(list_node.next, tree_node.right)
         )
