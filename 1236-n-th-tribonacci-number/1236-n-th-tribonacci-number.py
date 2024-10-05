79128651080f04b89e386f1e0bb6488f878ce58d
Kaleb Tedla <115733387+kalebted@users.noreply.github.com>
2024-10-05 20:30:27 +0300

    Time: 29 ms (87.52%), Space: 16.4 MB (65.39%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.py
@@ -1,6 +1,18 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        seq = [0,1,1]
+        # seq = [0,1,1]
+        # for i in range(3, n+1):
+        #     seq.append(seq[i-3] + seq[i-2] + seq[i-1])
+        # return seq[n]
+
+        if n == 0:
+            return 0
+        if n < 3:
+            return 1
+
+        t0, t1, t2 = 0, 1, 1
+
         for i in range(3, n+1):
-            seq.append(seq[i-3] + seq[i-2] + seq[i-1])
-        return seq[n]
+            tn = t0 + t1 + t2
+            t0, t1, t2 = t1, t2, tn
+        return tn
